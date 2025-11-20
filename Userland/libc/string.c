81f1a621ef3a641fef3d71a1274ef66dc45727d7
Rocco Perrone <theroccoperrone@gmail.com>
2025-11-19 21:00:27 -0300

    warnings

--- Userland/libc/string.c
+++ Userland/libc/string.c
@@ -13,7 +13,7 @@ int strlen(const char * str) {
     return i;
 }
 
-int strcmp(char * str1, char * str2) {
+int strcmp(const char * str1, const char * str2) {
     int i = 0;
     while (str1[i] != 0 && str2[i] != 0) {
         if (str1[i] != str2[i]) {
@@ -24,7 +24,7 @@ int strcmp(char * str1, char * str2) {
     return str1[i] - str2[i];
 }
 
-int strcasecmp(char * str1, char * str2) {
+int strcasecmp(const char * str1, const char * str2) {
     int i = 0;
     while (str1[i] != 0 && str2[i] != 0) {
         if (toupper(str1[i]) != toupper(str2[i])) {
@@ -35,7 +35,7 @@ int strcasecmp(char * str1, char * str2) {
     return str1[i] - str2[i];
 }
 
-void strcpy(char * dest, char * src) {
+void strcpy(char * dest, const char * src) {
     int i = 0;
     while (src[i] != 0) {
         dest[i] = src[i];
@@ -44,7 +44,7 @@ void strcpy(char * dest, char * src) {
     dest[i] = 0;
 }
 
-void strncpy(char * dest, char * src, int n) {
+void strncpy(char * dest, const char * src, int n) {
     int i = 0;
     while (i < n && src[i] != 0) {
         dest[i] = src[i];
